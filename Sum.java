cd2cf460b6014746e52c988b7cb3d4f522ab3eab
Vivek Kalal <vivekkalal20@gmail.com>
2022-02-23 00:46:49 +0530

    Added is in Sum.java

--- Sum.java
+++ Sum.java
@@ -1,6 +1,6 @@
 class Sum{
 	public static void main(String args[]){
 		int fno = 5 , sno = 10 ;
-		System.out.println("Sum :- " + (fno + sno));
+		System.out.println("Sum is :- " + (fno + sno));
 	}
 }
